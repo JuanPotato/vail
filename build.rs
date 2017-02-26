a234e7d75daa5eaa8267ffa21f5ece51c6ce38f3
Juan Potato <hasantiny@gmail.com>
2017-02-25 22:31:48 -0500

    Working on parser

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate regex;
 
 use regex::Regex;
+
+use std::collections::HashMap;
 
 use std::env;
 use std::fs::File;
@@ -13,21 +18,32 @@ use std::i64;
 struct Arg {
     name: String,
     arg_type: String,
-    bit_flag: i64,
+    flag_bit: i64,
 }
 
 #[derive(Debug)]
 struct TlItem {
     name: String,
-    id: i64,
+    id: u32,
     item_type: String,
     args: Option<Vec<Arg>>,
-    function: bool,
 }
 
+// Notes
+
+// # is a u32, only used for the flags attribute
+// args are flags.1?true which means if the 2nd bit (0 indexed) is 1,
+//   then there will be a true object. In this case if the bit is 1
+//   then we just assume it is true and go on rather than looking for a true id 
+// X, !X, and Type are generic, so anything
+// int is i32
+// long is i64
+// float is f32
+// double is f64
+
+
 fn main() {
-    let item_regex = Regex::new(r"^(?P<name>\w+)(?:\.(?P<sub>\w+))?#(?P<id>[0-9a-f]+) (?P<args>.*)= (?P<type>[\w<.>]+);").unwrap();
-    let args_regex = Regex::new(r"(?P<name>\w+):(?:flags\.(?P<bit_flag>\d+)\?)?(?P<type>[\w<.>#]+) ?").unwrap();
+    let item_regex = Regex::new(r"^(?P<name>[\w\.]+)#(?P<id>[0-9a-f]+) (?P<args>[\w <>:#?.{}!]*)= (?P<type>[\w<.>]+);").unwrap();
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("tl.rs");
@@ -37,8 +53,9 @@ fn main() {
 
     tl_scheme_file.read_to_string(&mut tl_scheme_contents);
 
-    let constructors: Vec<TlItem> = Vec::new();
-    let functions: Vec<TlItem> = Vec::new();
+    let mut constructors: Vec<TlItem> = Vec::new();
+    let mut functions: Vec<TlItem> = Vec::new();
+    let mut type_counts: HashMap<String, i64> = HashMap::new(); // how many types there are of each type
 
     let mut is_function = false;
     for line in tl_scheme_contents.lines() {
@@ -48,20 +65,73 @@ fn main() {
         }
 
         if let Some(captures) = item_regex.captures(line) {
-            let name = upper_first(captures.name("name").unwrap().as_str());
-            let sub = captures.name("sub");
-            let id = i64::from_str_radix(captures.name("id").unwrap().as_str(), 16).unwrap();
+            let name = dot_to_camel(captures.name("name").unwrap().as_str());
+            let id = u32::from_str_radix(captures.name("id").unwrap().as_str(), 16).unwrap();
             let args = captures.name("args");
-            let item_type = captures.name("type").unwrap().as_str().to_string();
+            let item_type = dot_to_camel(captures.name("type").unwrap().as_str());
 
             let mut item = TlItem {
-                name: if sub.is_some() { name + sub.unwrap().as_str() } else { name },
+                name: name,
                 id: id,
-                item_type: item_type,
-                args: None,
-                function: is_function,
+                item_type: item_type.clone(),
+                args: parse_args(args),
             };
+
+            if is_function {
+                functions.push(item);
+            } else {
+                constructors.push(item);
+                let counter = type_counts.entry(item_type).or_insert(0);
+                *counter += 1;
+            }
         }
+    }
+
+    println!("{:#?}", type_counts);
+
+    // for mut ref cons in constructors {
+
+    // }
+}
+
+fn parse_args(capture: Option<regex::Match>) -> Option<Vec<Arg>> {
+    lazy_static! {
+        static ref ARG_RE: Regex = Regex::new(r"(?P<name>\w+):(?:flags\.(?P<flag_bit>\d+)\?)?(?P<type>[\w<.>#]+) ?").unwrap();
+    }
+    match capture {
+        Some(cap) => {
+            let mut args: Vec<Arg> = Vec::new();
+            
+            for piece in cap.as_str().trim().split_whitespace() {
+                if piece.contains('{') {
+                    // This arg was something like {X:Type} which we take into account already
+                    continue;
+                }
+                if let Some(capture) = ARG_RE.captures(piece) {
+                    let name = capture.name("name").unwrap().as_str().to_string();
+                    let arg_type = dot_to_camel(capture.name("type").unwrap().as_str());
+                    
+                    let flag_bit = if let Some(bit) = capture.name("flag_bit") {
+                        bit.as_str().parse::<i64>().unwrap()
+                    } else {
+                        -1
+                    };
+
+                    args.push(Arg {
+                        name: name,
+                        arg_type: arg_type,
+                        flag_bit: flag_bit,
+                    });
+                }
+            }
+
+            if args.len() == 0 {
+                None
+            } else {
+                Some(args)
+            }
+        },
+        None => None,
     }
 }
 
@@ -69,6 +139,10 @@ fn upper_first(s: &str) -> String {
     let mut c = s.chars();
     match c.next() {
         None => String::new(),
-        Some(f) => f.to_uppercase().chain(c).collect(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+fn dot_to_camel(s: &str) -> String {
+    s.split('.').map(|a| upper_first(a)).collect::<String>()
 }
