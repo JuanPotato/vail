16e54edfdadbb9332b536f51bacf83f81e54d158
Juan Potato <hasantiny@gmail.com>
2017-03-08 21:54:18 -0500

    Fix string and begin Deserialization

--- vail/src/lib.rs
+++ vail/src/lib.rs
@@ -2,13 +2,17 @@
 extern crate tl_derive;
 extern crate byteorder;
 
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{LittleEndian, WriteBytesExt, ReadBytesExt};
 use std::io::{Cursor, Write};
 
 mod tl;
 
-trait Serialize<T> {
-    fn serialize(&mut self, obj: &T);
+trait Serialize<S> {
+    fn serialize(&mut self, obj: &S);
+}
+
+trait Deserialize<D> {
+    fn deserialize(&self) -> D;
 }
 
 impl Serialize<bool> for Cursor<Vec<u8>> {
@@ -21,9 +25,25 @@ impl Serialize<bool> for Cursor<Vec<u8>> {
     }
 }
 
+impl Deserialize<bool> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> bool {
+        if self.read_u32::<LittleEndian>().unwrap() == 0xbc799737 {
+            false
+        } else {
+            true
+        }
+    }
+}
+
 impl Serialize<u32> for Cursor<Vec<u8>> {
     fn serialize(&mut self, obj: &u32) {
         self.write_u32::<LittleEndian>(*obj).unwrap();
+    }
+}
+
+impl Deserialize<u32> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> u32 {
+        self.read_u32::<LittleEndian>().unwrap()
     }
 }
 
@@ -33,9 +53,21 @@ impl Serialize<i32> for Cursor<Vec<u8>> {
     }
 }
 
+impl Deserialize<i32> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> i32 {
+        self.read_i32::<LittleEndian>().unwrap()
+    }
+}
+
 impl Serialize<f32> for Cursor<Vec<u8>> {
     fn serialize(&mut self, obj: &f32) {
         self.write_f32::<LittleEndian>(*obj).unwrap();
+    }
+}
+
+impl Deserialize<f32> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> f32 {
+        self.read_f32::<LittleEndian>().unwrap()
     }
 }
 
@@ -45,9 +77,21 @@ impl Serialize<i64> for Cursor<Vec<u8>> {
     }
 }
 
+impl Deserialize<i64> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> i64 {
+        self.read_i64::<LittleEndian>().unwrap()
+    }
+}
+
 impl Serialize<f64> for Cursor<Vec<u8>> {
     fn serialize(&mut self, obj: &f64) {
         self.write_f64::<LittleEndian>(*obj).unwrap();
+    }
+}
+
+impl Deserialize<f64> for Cursor<Vec<u8>> {
+    fn deserialize(&self) -> f64 {
+        self.read_f64::<LittleEndian>().unwrap()
     }
 }
 
@@ -59,12 +103,12 @@ impl Serialize<Vec<u8>> for Cursor<Vec<u8>> {
             let _ = self.write(&[len as u8]);
             len += 1; 
         } else {
-            let _ = self.write_all(&[len as u8, (len >> 8) as u8, (len >> 16) as u8]); // 3 bytes
+            let _ = self.write_all(&[254, len as u8, (len >> 8) as u8, (len >> 16) as u8]); // 3 bytes
         }
 
         let _ = self.write_all(obj);
 
-        for _ in 0 .. (4 - len % 4) {
+        for _ in 0 .. (4 - len) % 4 {
             let _ = self.write(&[00u8]);
         }
     }
@@ -78,12 +122,12 @@ impl Serialize<String> for Cursor<Vec<u8>> {
             let _ = self.write(&[len as u8]);
             len += 1; 
         } else {
-            let _ = self.write_all(&[len as u8, (len >> 8) as u8, (len >> 16) as u8]); // 3 bytes
+            let _ = self.write_all(&[254, len as u8, (len >> 8) as u8, (len >> 16) as u8]); // 3 bytes
         }
 
         let _ = self.write_all(obj.as_bytes());
 
-        for _ in 0 .. (4 - len % 4) {
+        for _ in 0 .. (4 - len) % 4 {
             let _ = self.write(&[00u8]);
         }
     }
@@ -110,7 +154,10 @@ impl<T> Serialize<Box<T>> for Cursor<Vec<u8>> where
 
 #[test]
 fn test() {
-    let mut buf: Cursor<Vec<u8>> = Cursor::new(Vec::new());
-    buf.serialize(String::from("Potato"));
-    println!("{:?}", buf);
+    let s = "123";
+    for i in 0..s.len() {
+        let mut buf: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+        buf.serialize(&(s[0..(i+1)].to_string()));
+        println!("{:?}", buf);
+    }
 }
